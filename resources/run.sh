879a2de38fcd7380ad63998dc3d2f2c51f84a31a
HEEJAERO <rhj0830@gmail.com>
2023-02-28 21:20:10 +0900

    Feat: 모든 오브젝트 추가

---
+++ resources/run.sh
@@ -0,0 +1,32 @@
+echo "Auth Deploy Start!!"
+kubectl apply -f auth/ns-auth.yaml
+kubectl apply -f auth/auth-cm.yaml
+kubectl apply -f auth/auth.yaml
+kubectl apply -f auth/auth-redis.yaml
+
+echo "Member Deploy Start!!"
+kubectl apply -f member/ns-member.yaml
+kubectl apply -f member/member-cm.yaml
+kubectl apply -f member/member-db.yaml
+kubectl apply -f member/member.yaml
+
+echo "Performance Deploy Start!!"
+kubectl apply -f performance/ns-performance.yaml
+kubectl apply -f performance/performance-configmap.yaml
+kubectl apply -f performance/performance-db.yaml
+kubectl apply -f performance/performance-deploy.yaml
+kubectl apply -f performance/performance-redis.yaml
+kubectl apply -f performance/performance-secret.yaml
+kubectl apply -f performance/performance-svc.yaml
+
+echo "Reservation Deploy Start!!"
+
+kubectl apply -f reservation/reservation.yml
+echo "Front Deploy Start!!"
+
+kubectl apply -f front/ns-ticketaka-front.yaml
+kubectl apply -f front/ticketaka-front-cm.yaml
+kubectl apply -f front/ticketaka-front-deploy.yaml
+kubectl apply -f front/ticketaka-front-secret.yaml
+kubectl apply -f front/ticketaka-front-svc.yaml
+
